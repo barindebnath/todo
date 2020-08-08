536dfad19a267574a325feb0f5b7a6887598c141
Barin <barindebnath@gmail.com>
2020-08-08 11:57:54 +0530

    Updates

---
+++ precache-manifest.66bd8373808419cd85af17629f298e0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb46c9b0e641fbedf10b683e8f4b767c",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "bca658aef5cee5ed34d6",
+    "url": "/todo/static/css/main.26365fb9.chunk.css"
+  },
+  {
+    "revision": "411071cd63858b372a96",
+    "url": "/todo/static/js/2.41b1d433.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.41b1d433.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bca658aef5cee5ed34d6",
+    "url": "/todo/static/js/main.ef46a11e.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
